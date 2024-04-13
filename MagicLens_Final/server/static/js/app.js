474bc9a1072de4c5651fdd765465c42df20487a5
Shreya Nair <33029951+shreya-001@users.noreply.github.com>
2024-04-13 05:03:02 -0400

    Update app.js

--- MagicLens_Final/server/static/js/app.js
+++ MagicLens_Final/server/static/js/app.js
@@ -18,7 +18,7 @@ function init() {
     dz.on("complete", function (file) {
         let imageData = file.dataURL;
         
-        var url = "http://127.0.0.1:5000/classify_image";
+        var url = "http://0.0.0.0:10000/classify_image";
 
         $.post(url, {
             image_data: file.dataURL
